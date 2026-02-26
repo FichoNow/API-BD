dee2a1f75f8a59a487dd7628f24a722c28721bb9
banlles <janbanlles@gmail.com>
2026-02-26 20:24:04 +0100

    feat: agregar configuración de express y middleware para manejar JSON

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
-function main(): void {
-  console.log("API iniciada 🚀");
-}
+import express, { Request, Response } from "express";
 
-main();
+const app = express();
+
+// Middleware para leer JSON
+app.use(express.json());
+
+// Ruta GET simple
+app.get("/", (_req: Request, res: Response) => {
+  res.json({
+    message: "API funcionando 🚀",
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Servidor corriendo en http://localhost:3000`);
+});
