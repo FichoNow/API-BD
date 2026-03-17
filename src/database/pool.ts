f1fd72608116e506d9dba5f73f5c6a90077cc64f
banlles <janbanlles@gmail.com>
2026-03-18 00:00:21 +0100

    Agregar soporte para variables de entorno utilizando dotenv: se actualizan package.json y package-lock.json, se modifica pool.ts y server.ts para usar variables de entorno, y se crea un archivo .env.example.

--- src/database/pool.ts
+++ src/database/pool.ts
@@ -1,11 +1,12 @@
+import "dotenv/config";
 import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
   host: "localhost",
-  port: Number(3306),
-  user: "root",
-  password: "linux123",
-  database: "BD_Jan_Fran",
+  port: Number(process.env.DATABASE_PORT),
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,7 @@
+import "dotenv/config";
 import { app } from "./app.js";
 
-const PORT = 3000;
-
-// Log para verificar que nuestro server.ts se ejecuta!
-console.log("DEBUG: server.ts cargado");
-
 // Arranque del servidor :D
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http:localhost:${PORT}`);
+app.listen(process.env.API_PORT, () => {
+  console.log(`Servidor escuchando en http:localhost:${process.env.API_PORT}`);
 });
