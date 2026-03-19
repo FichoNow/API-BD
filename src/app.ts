cafef0733fbd11477fcb545996a286def57532e3
banlles <janbanlles@gmail.com>
2026-03-19 01:21:37 +0100

    Renombrar middleware de manejo de errores: cambiar errorHandler a errorMiddleware en app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authRouter } from "./api/routes/auth-routes.js";
-import { errorHandler } from "./api/middleware/error-handler.js";
+import { errorMiddleware } from "./api/middleware/error-middleware.js";
 
 export const app = express();
 
@@ -8,4 +8,4 @@ app.use(express.json());
 
 app.use("/auth", authRouter);
 
-app.use(errorHandler);
+app.use(errorMiddleware);
