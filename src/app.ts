7b1e1350fc9973d1605940488b84045f718d3138
banlles <janbanlles@gmail.com>
2026-05-03 13:52:30 +0200

    feat: eliminar rutas de prueba y su uso en el servidor

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,7 @@ import { superadminRouter } from "./api/routes/superadmin-routes.js";
 import { requireSuperAdmin } from "./api/middleware/superadmin-authenticate-middleware.js";
 import { userRouter } from "./api/routes/user-routes.js";
 import { requireAuth } from "./api/middleware/user-authenticate-middleware.js";
-import { testRouter } from "./api/routes/test-routes.js";
 import { swaggerDocs } from "./docs/swagger.js";
-import { env } from "./config-env.js";
 
 export const app = express();
 
@@ -26,9 +24,5 @@ app.use("/admin", requireAdministrator, adminRouter);
 app.use("/superadmin", requireSuperAdmin, superadminRouter);
 
 app.use("/user", requireAuth, userRouter);
-
-if (env.NODE_ENV === "development") {
-  app.use("/test", testRouter);
-}
 
 app.use(errorMiddleware);
